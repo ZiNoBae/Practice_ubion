ee0117fa3591abf5d133c58638627db898e06dee
ZiNoBae <129731827+ZiNoBae@users.noreply.github.com>
2023-04-06 17:31:29 +0900

    0406class

---
+++ Code_Moon/230406/list_load/__init__.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import os
+
+# 함수의 형태로 폴더에 있는 파일들 결합하는 함수 생성
+
+def load(_dir, _end = '.csv') : 
+    # 매개변수 2개 생성 : dir -> 파일의 경로 (마지막에 / 붙이는거 주의)
+                        # _end -> 파일의 확장자 (처음에 . 붙이는거 주의)
+
+    if ~(_dir.endswith('/')) :
+        _dir = _dir + '/'
+
+    if _end[0] != '.' :
+        _end = '.' + _end
+
+    #print(_dir, _end)
+
+
+    #파일의 리스트를 호출 
+    file_list = os.listdir(_dir)
+    # print(file_list)
+
+    # 비어있는 데이터 프레임 생성
+    result = pd.DataFrame()
+    
+    #파일 리스트 중 확장자가 같은 파일들만 결합 
+    for i in file_list : 
+        # 파일명의 확장자라는 인자값과 같은 경우
+        if i.endswith(_end) : 
+            print(i)
+            #확장자에 따른 read함수 설정
+            if _end == '.csv' :
+                    df = pd.read_csv(_dir + i)
+                    result = pd.concat([result, df], axis = 0, ignore_index = True)
+
+            elif _end == '.json' :
+                    df = pd.read_json(_dir + i) 
+                    result = pd.concat([result, df], axis = 0, ignore_index = True)
+
+            # elif (_end == '.xls') or (_end == '.xlsx') :   #2개일때는 이렇게 쓰면 되는데
+            elif _end in ['.xlsx', '.xls'] :  # 그 이상 여러개일때는, 이게 더 편하다 
+                    df = pd.read_excel(_dir+i)
+                    result = pd.concat([result, df], axis = 0, ignore_index= True)
+            else :
+                return "지원하지 않는 확장자입니다."
+    return result
