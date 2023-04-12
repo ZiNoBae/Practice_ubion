2dfe89b5abbe9c4a3471f2d60fabd0941c71768b
ZiNoBae <129731827+ZiNoBae@users.noreply.github.com>
2023-04-12 17:24:36 +0900

    0412_class

--- Code_Moon/230412/buyandhold.py
+++ Code_Moon/230412/buyandhold.py
@@ -1,12 +1,22 @@
 import numpy as np
-
-
-def bnh(x, col) : 
-    x = x.loc[~x.isin([np.nan, np.inf, -np.inf]).any(1)]
-    x = x[[col]]
-    x['daily_rtn'] = x[col].pct_change()
-    x['cumulative_rtn'] = (1 + x['daily_rtn']).cumprod()
+from datetime import datetime
+import pandas as pd
 
 
 
-    return x
+
+def bnh(df, col, start, end) :
+    # 인덱스 시계열로 변경
+    df.index = pd.to_datetime(df.index)
+    start = datetime.strptime(start, '%Y%m%d').isoformat()
+    end = datetime.strptime(end, '%Y%m%d').isoformat()
+    df = df.loc[start : end]
+
+    df = df.loc[~df.isin([np.nan, np.inf, -np.inf]).any(1)]
+    df = df[[col]]
+    df['daily_rtn'] = df[col].pct_change()
+    df['cumulative_rtn'] = (1 + df['daily_rtn']).cumprod()
+
+
+
+    return df
